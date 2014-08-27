bc45983bb197b881a65d32024b13ce0268031d11
Tobias Ziegler <t.ziegler@ids-system.de>
2014-08-27 00:53:32 -0700

    New functionality change chapter and language track

--- routes.py
+++ routes.py
@@ -6,10 +6,18 @@ import glob
 import os
 import sys
 import subprocess, signal
+import stat
 
 app = Flask(__name__)
 app.secret_key = 'some_secret'
 app.path= "/mnt/Movies"
+app.fifo= "/home/tozi/omx.pipe"
+def fifo():
+    if os.path.exists(app.fifo):
+        pass
+    else:
+        os.system("mkfifo "+app.fifo)
+
 
 @app.route('/')
 def home():
@@ -33,6 +41,7 @@ def home():
   return render_template('home.html',names=names, i=i, thumbnails=thumbnails)
 
 
+
 @app.route('/movie', methods=['GET'])
 def movie():
     name=str(request.args.get('movieid').encode('utf-8'))
@@ -40,7 +49,8 @@ def movie():
         if os.path.exists(app.path+"/"+name):
             os.chdir(app.path+"/"+name)
             for movie in glob.glob("*.mkv"):
-                os.system("omxplayer -b -o hdmi "+movie+" &")
+                os.system("omxplayer -b -o hdmi "+movie+" < "+app.fifo +" &")
+                os.system("echo -n . > " + app.fifo)
     return render_template('movie.html', name=name)
 
 @app.route('/kill')
@@ -55,5 +65,46 @@ def kill():
             os.kill(pid, signal.SIGKILL)
     return redirect(url_for('home'))
 
+@app.route('/_pause')
+def pause():
+    if request.args.get('state') == "paused":
+        result="Pause"
+    else:
+        result="Play"
+    os.system("echo -n p >"+app.fifo)
+    return jsonify(result)
+
+
+@app.route('/_pChapter')
+def pChapter():
+    if request.args.get('state') == "paused":
+        result="Pause"
+    else:
+        result="Play"
+    os.system("echo -n i >"+app.fifo)
+    return jsonify(result)
+
+@app.route('/_nChapter')
+def nChapter():
+    result="Pause"
+    os.system("echo -n o >"+app.fifo)
+    return jsonify(result)
+
+@app.route('/_nLang')
+def nLang():
+    result="Pause"
+    os.system("echo -n k >"+app.fifo)
+    return jsonify(result)
+
+@app.route('/_pLang')
+def pLang():
+    result="Pause"
+    os.system("echo -n j >"+app.fifo)
+    return jsonify(result)
+
+
+
+
 if __name__ == '__main__':
+  fifo()
   app.run(host='0.0.0.0')
